362c786229f73aca83cd2246922b27192df45765
Gabe S <gabesliwowicz@gmail.com>
2024-01-05 15:42:11 -0500

    Poster/user can delete posts and edit posts.

--- controllers/blogpostController.js
+++ controllers/blogpostController.js
@@ -1,6 +1,7 @@
 const Post = require("../models/post");
 const Comment = require("../models/comment");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require('express-validator');
 
 exports.index = asyncHandler(async (req, res, next) => {
   // Get details of posts
@@ -41,30 +42,140 @@ exports.post_detail = asyncHandler(async (req, res, next) => {
 
 // Display post create form on GET.
 exports.post_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: post create GET");
+  res.render("post_form", { title: "Create Post", c_user: req.user });
 });
 
 // Handle post create on POST.
-exports.post_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: post create POST");
-});
+exports.post_create_post = [
+  // Validate and sanitize the username field.
+  body("title")
+  .trim()
+  .isLength({ min: 3, max: 25 })
+  .escape()
+  .withMessage("Title must be between 3-25 characters."),
+  body("text")
+  .trim()
+  .isLength({ min: 20, max: 200 })
+  .escape()
+  .withMessage("Post must be between 20-200 characters."),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      res.render("post_form", { title: "Create Post", c_user: req.user, errors: errors.array(), message: req.body })
+      return;
+    }
+
+    const post = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        timestamp: new Date(),
+    })
+
+    await post.save();
+    res.redirect(post.url);
+  })
+]
 
 // Display post delete form on GET.
 exports.post_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: post delete GET");
+  const post = await Post.findById(req.params.id).exec();
+
+  if(post === null) {
+    const err = new Error("Post not found.");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("post_delete", {
+    title: "Delete Post",
+    c_user: req.user,
+    post: post
+  })
 });
 
 // Handle post delete on POST.
 exports.post_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: post delete POST");
+  const post = await Post.findById(req.params.id).exec();
+
+  if(!req.user) {
+    res.status(403).send("Unauthorized");
+    return;
+  }
+  
+  if(post === null) {
+    const err = new Error("Post not found.");
+    err.status = 404;
+    return next(err);
+  } else {
+    await Post.findByIdAndDelete(req.body.postid);
+    res.redirect("/");
+  }
 });
 
 // Display post update form on GET.
 exports.post_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: post update GET");
+  const post = await Post.findById(req.params.id).exec();
+
+  if(post === null) {
+    const err = new Error("Post not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("post_form", {
+    title: "Update Form",
+    c_user: req.user,
+    post: post,
+  })
 });
 
 // Handle post update on POST.
-exports.post_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: post update POST");
-});
+exports.post_update_post = [ 
+  // Validate and sanitize the username field.
+  body("title")
+  .trim()
+  .isLength({ min: 3, max: 25 })
+  .escape()
+  .withMessage("Title must be between 3-25 characters."),
+  body("text")
+  .trim()
+  .isLength({ min: 20, max: 200 })
+  .escape()
+  .withMessage("Post must be between 20-200 characters."),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      res.render("post_form", { title: "Create Post", c_user: req.user, errors: errors.array(), message: req.body })
+      return;
+    }
+
+    const originalPost = await Post.findById(req.params.id);
+
+    // Only a blog poster can edit a post.
+    if(!originalPost || !req.user) {
+      res.status(403).send("Unauthorized");
+      return;
+    }
+
+    const post = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        timestamp: new Date(),
+        _id: req.params.id,
+    })
+    if(!errors.isEmpty()) {
+      res.render("post_form", {
+        title: "Update Form",
+        post: post,
+        c_user: req.user,
+        errors: errors.array(),
+      })
+      return;
+    } else {
+      const updatedPost = await Post.findByIdAndUpdate(req.params.id, post, {});
+      res.redirect(updatedPost.url);
+    }
+  })
+]
