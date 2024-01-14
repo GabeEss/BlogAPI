caa0a000d9a2590ea2fae925c68fa25e1b27b7aa
Gabe S <gabesliwowicz@gmail.com>
2024-01-14 15:51:22 -0500

    vite.config.js changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5000,
-    open: true,
-    proxy: {
-      '/blog': import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000',
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 5000,
+      open: true,
+      proxy: {
+        '/blog': process.env.VITE_BACKEND_URL || 'http://localhost:3000',
+      },
     },
-  },
-})
+  });
+};
